8c53002a20e3b702f75b3bbc145ca3fde0e7df50
odettelittle <Odettelittle1@gmail.com>
2022-03-31 01:03:30 +0000

    primerocommit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [task, setTask] = useState("");
+  const [list, setlist] = useState([]);
+  return (
+    <>
+      <div>
+        <h1>Lista de Tareas </h1>
+        <input
+          type="text"
+          placeholder="add task"
+          onChange={(e) => {
+            if (task !== "" && e.key === "Enter") {
+              setlist();
+              setTask("");
+            }
+          }}
+          value={task}
+        />
+        <button
+          onClick={() => {
+            setList([...list, task]);
+            setTask("");
+          }}
+        >
+          Submit
+        </button>
+
+        <ul>
+          {list.map((listItem, index) => {
+            return (
+              <li key={index} className="d-flex flex row">
+                <p> {listItem} </p>
+                <button
+                  onClick={() => {
+                    setList(list.filter((item) => item !== listItem));
+                  }}
+                >
+                  Delete
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default Home;
