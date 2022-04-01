e24a4c105124c5c358c3ec6864c7be720f4c0dad
odettelittle <Odettelittle1@gmail.com>
2022-04-01 20:59:26 +0000

    segundocommit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,51 +5,51 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-  const [task, setTask] = useState("");
-  const [list, setlist] = useState([]);
-  return (
-    <>
-      <div>
-        <h1>Lista de Tareas </h1>
-        <input
-          type="text"
-          placeholder="add task"
-          onChange={(e) => {
-            if (task !== "" && e.key === "Enter") {
-              setlist();
-              setTask("");
-            }
-          }}
-          value={task}
-        />
-        <button
-          onClick={() => {
-            setList([...list, task]);
-            setTask("");
-          }}
-        >
-          Submit
-        </button>
+	const [task, setTask] = useState("");
+	const [list, setlist] = useState([]);
+	return (
+		<>
+			<div>
+				<h1>Lista de Tareas </h1>
+				<input
+					type="text"
+					placeholder="Add Task"
+					onChange={(e) => {
+							setTask(e.target.value)
+						}
+					}
+					value={task}
+				/>
+				<button
+					onClick={() => {
+						setList([...list, task]);
+						setTask("");
+					}}>
+					Submit
+				</button>
 
-        <ul>
-          {list.map((listItem, index) => {
-            return (
-              <li key={index} className="d-flex flex row">
-                <p> {listItem} </p>
-                <button
-                  onClick={() => {
-                    setList(list.filter((item) => item !== listItem));
-                  }}
-                >
-                  Delete
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </>
-  );
+				<ul>
+					{list.map((listItem, index) => {
+						return (
+							<li key={index} className="d-flex flex row">
+								<p> {listItem} </p>
+								<button
+									onClick={() => {
+										setList(
+											list.filter(
+												(item) => item !== listItem
+											)
+										);
+									}}>
+									Delete
+								</button>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		</>
+	);
 };
 
 export default Home;
